edbe74b2f3573a2ca6b9016d3aa134d7c22eefd9
Fulton Byrne <fulton.byrne@consensys.net>
2018-04-11 16:30:30 -0400

    feat: consortium create, read, delete

---
+++ kaleido/consortium.go
@@ -0,0 +1,55 @@
+package kaleido
+
+import (
+	"fmt"
+
+	"gopkg.in/resty.v1"
+)
+
+const (
+	SingleOrg      = "single-org"
+	MultiOrg       = "multi-org"
+	DELETE_PENDING = "delete_pending"
+	DELETED        = "deleted"
+)
+
+type KaleidoClient struct {
+	Client *resty.Client
+}
+
+type Consortium struct {
+	Id          string `json:"_id,omitempty"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Mode        string `json:"mode"`
+	DeletedAt   string `json:"deleted_at,omitempty"`
+	State       string `json:"state,omitempty"`
+}
+
+func NewConsortium(name, description, mode string) Consortium {
+	return Consortium{
+		Id:          "",
+		Name:        name,
+		Description: description,
+		Mode:        mode,
+		DeletedAt:   "",
+		State:       "",
+	}
+}
+
+func NewClient(api string, apiKey string) KaleidoClient {
+	r := resty.New().SetHostURL(api).SetAuthToken(apiKey)
+	return KaleidoClient{r}
+}
+
+func (c *KaleidoClient) CreateConsortium(consortium *Consortium) (*resty.Response, error) {
+	return c.Client.R().SetBody(consortium).Post("/consortia")
+}
+
+func (c *KaleidoClient) ListConsortium(resultBox *[]Consortium) (*resty.Response, error) {
+	return c.Client.R().SetResult(resultBox).Get("/consortia")
+}
+
+func (c *KaleidoClient) DeleteConsortium(consortium *Consortium) (*resty.Response, error) {
+	return c.Client.R().Delete(fmt.Sprintf("/consortia/%s", consortium.Id))
+}
---
+++ kaleido/consortium_test.go
@@ -0,0 +1,42 @@
+package kaleido
+
+import (
+	"os"
+	"testing"
+)
+
+func TestConsortiumCreationListDeletion(t *testing.T) {
+	client := NewClient(os.Getenv("KALEIDO_API"), os.Getenv("KALEIDO_API_KEY"))
+	consortium := NewConsortium("testConsortium", "test description", "single-org")
+	res, err := client.CreateConsortium(&consortium)
+	if res.StatusCode() != 201 {
+		t.Fatalf("Could not create consortium status code: %d.", res.StatusCode())
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	var consortia []Consortium
+	_, err = client.ListConsortium(&consortia)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	//Check for a newly created consortia and delete it.
+	countNew := 0
+	for _, x := range consortia {
+		t.Logf("\n%v", x)
+		if x.Name == "testConsortium" && (x.State != DELETED && x.State != DELETE_PENDING) {
+			res, err = client.DeleteConsortium(&x)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if res.StatusCode() != 202 {
+				t.Errorf("Consortium Deletion Failed Status %d.", res.StatusCode())
+			}
+			countNew += 1
+			t.Logf("\nNew Consortium: %v", x)
+		}
+	}
+}
