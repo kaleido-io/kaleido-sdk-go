922077f6326a4746815300573adcfd5e78dbfdbd
Azeem Ahmed <azeem.ahmed@consensys.net>
2018-10-10 20:08:24 -0500

    fixed printf modifier

--- cmd/consortium.go
+++ cmd/consortium.go
@@ -14,97 +14,97 @@
 package cmd
 
 import (
-  "encoding/json"
-  "fmt"
-  "os"
+	"encoding/json"
+	"fmt"
+	"os"
 
-  kld "github.com/kaleido-io/kaleido-sdk-go/kaleido"
-  "github.com/spf13/cobra"
+	kld "github.com/kaleido-io/kaleido-sdk-go/kaleido"
+	"github.com/spf13/cobra"
 )
 
 var consortiumListCmd = &cobra.Command{
-  Use: "consortium",
-  Short: "List the consortiums under the user's account",
-  Run: func(cmd *cobra.Command, args []string) {
-    client := getNewClient()
-    var consortiums []kld.Consortium
-    _, err := client.ListConsortium(&consortiums)
-    if err != nil {
-      fmt.Printf("Failed to list consortiums. %v\n", err)
-      os.Exit(1)
-    }
+	Use:   "consortium",
+	Short: "List the consortiums under the user's account",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := getNewClient()
+		var consortiums []kld.Consortium
+		_, err := client.ListConsortium(&consortiums)
+		if err != nil {
+			fmt.Printf("Failed to list consortiums. %v\n", err)
+			os.Exit(1)
+		}
 
-    encoded, _ := json.Marshal(consortiums)
-    fmt.Printf("\n%+v\n", string(encoded))
-  },
+		encoded, _ := json.Marshal(consortiums)
+		fmt.Printf("\n%+v\n", string(encoded))
+	},
 }
 
 var consortiumGetCmd = &cobra.Command{
-  Use: "consortium",
-  Short: "Get the consortium details",
-  Run: func(cmd *cobra.Command, args []string) {
-    client := getNewClient()
-    var consortium kld.Consortium
-    res, err := client.GetConsortium(consortiumId, &consortium)
-    validateGetResponse(res, err, "consortium")
-  },
+	Use:   "consortium",
+	Short: "Get the consortium details",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := getNewClient()
+		var consortium kld.Consortium
+		res, err := client.GetConsortium(consortiumId, &consortium)
+		validateGetResponse(res, err, "consortium")
+	},
 }
 
 var consortiumCreateCmd = &cobra.Command{
-  Use: "consortium",
-  Short: "Create a consortium",
-  Run: func(cmd *cobra.Command, args []string) {
-    validateName()
+	Use:   "consortium",
+	Short: "Create a consortium",
+	Run: func(cmd *cobra.Command, args []string) {
+		validateName()
 
-    if mode != "single-org" && mode != "multi-org" {
-      fmt.Printf("Invalid consortium mode: %n\n", mode)
-      os.Exit(1)
-    }
+		if mode != "single-org" && mode != "multi-org" {
+			fmt.Printf("Invalid consortium mode: %s\n", mode)
+			os.Exit(1)
+		}
 
-    client := getNewClient()
-    consortium := kld.NewConsortium(name, desc, mode)
-    res, err := client.CreateConsortium(&consortium)
+		client := getNewClient()
+		consortium := kld.NewConsortium(name, desc, mode)
+		res, err := client.CreateConsortium(&consortium)
 
-    validateCreationResponse(res, err, "consortium")
-  },
+		validateCreationResponse(res, err, "consortium")
+	},
 }
 
 var consortiumDeleteCmd = &cobra.Command{
-  Use: "consortium",
-  Short: "Delete a consortium",
-  Run: func(cmd *cobra.Command, args []string) {
-    validateDeleteId("consortium")
+	Use:   "consortium",
+	Short: "Delete a consortium",
+	Run: func(cmd *cobra.Command, args []string) {
+		validateDeleteId("consortium")
 
-    client := getNewClient()
-    res, err := client.DeleteConsortium(deleteId)
+		client := getNewClient()
+		res, err := client.DeleteConsortium(deleteId)
 
-    validateDeletionResponse(res, err, "consortium")
-  },
+		validateDeletionResponse(res, err, "consortium")
+	},
 }
 
 func newConsortiumGetCmd() *cobra.Command {
-  flags := consortiumGetCmd.Flags()
-  flags.StringVar(&consortiumId, "id", "", "Id of the consortium to retrieve")
+	flags := consortiumGetCmd.Flags()
+	flags.StringVar(&consortiumId, "id", "", "Id of the consortium to retrieve")
 
-  return consortiumGetCmd
+	return consortiumGetCmd
 }
 
 func newConsortiumListCmd() *cobra.Command {
-  return consortiumListCmd
+	return consortiumListCmd
 }
 
 func newConsortiumCreateCmd() *cobra.Command {
-  flags := consortiumCreateCmd.Flags()
-  flags.StringVarP(&name, "name", "n", "", "Name of the consortium")
-  flags.StringVarP(&desc, "desc", "d", "", "Short description of the purpose of the consortium")
-  flags.StringVarP(&mode, "mode", "m", "single-org", "single-org or multi-org consortium")
+	flags := consortiumCreateCmd.Flags()
+	flags.StringVarP(&name, "name", "n", "", "Name of the consortium")
+	flags.StringVarP(&desc, "desc", "d", "", "Short description of the purpose of the consortium")
+	flags.StringVarP(&mode, "mode", "m", "single-org", "single-org or multi-org consortium")
 
-  return consortiumCreateCmd
+	return consortiumCreateCmd
 }
 
 func newConsortiumDeleteCmd() *cobra.Command {
-  flags := consortiumDeleteCmd.Flags()
-  flags.StringVar(&deleteId, "id", "", "Id of the consortium to delete")
+	flags := consortiumDeleteCmd.Flags()
+	flags.StringVar(&deleteId, "id", "", "Id of the consortium to delete")
 
-  return consortiumDeleteCmd
+	return consortiumDeleteCmd
 }
