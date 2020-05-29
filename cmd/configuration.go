13cafc4423b3f2ca91e0702f35f2aa87dddf463f
Peter Broadhurst <peter.broadhurst@kaleido.io>
2020-05-29 11:45:11 -0400

    Fix review comment

--- cmd/configuration.go
+++ cmd/configuration.go
@@ -137,7 +137,7 @@ func newConfigurationGetCmd() *cobra.Command {
 	flags := configGetCmd.Flags()
 	flags.StringVarP(&consortiumID, "consortium", "c", "", "ID of the consortium to to get the configuration")
 	flags.StringVarP(&environmentID, "environment", "e", "", "ID of the environment to get the configuration")
-	flags.StringVarP(&configID, "configuration", "i", "", "ID of the configuration")
+	flags.StringVarP(&configID, "id", "i", "", "ID of the configuration")
 	return configGetCmd
 }
 
@@ -145,7 +145,7 @@ func newConfigurationDeleteCmd() *cobra.Command {
 	flags := configDeleteCmd.Flags()
 	flags.StringVarP(&consortiumID, "consortium", "c", "", "ID of the consortium to to delete the configurations")
 	flags.StringVarP(&environmentID, "environment", "e", "", "ID of the environment to delete the configurations")
-	flags.StringVarP(&configID, "configuration", "i", "", "ID of the configuration")
+	flags.StringVarP(&configID, "id", "i", "", "ID of the configuration")
 	return configDeleteCmd
 }
 
